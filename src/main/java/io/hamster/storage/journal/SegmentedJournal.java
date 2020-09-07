5028b62c7ba038b5402ff156d608ded8eeb76bfd
liuyong1352@126.com <github@@159874>
2020-09-07 22:03:40 +0800

    commit journal segment writer

--- src/main/java/io/hamster/storage/journal/SegmentedJournal.java
+++ src/main/java/io/hamster/storage/journal/SegmentedJournal.java
@@ -1,6 +1,31 @@
 package io.hamster.storage.journal;
 
-public class SegmentedJournal {
+import java.io.File;
 
+public class SegmentedJournal<E> {
 
+    private final File root;
+    private final String name;
+    private final int maxEntrySize;
+    private JournalSegment<E> current;
+
+    public SegmentedJournal(
+            String name,
+            File root,
+            int maxEntrySize) {
+        this.name = name;
+        this.root = root;
+        this.maxEntrySize = maxEntrySize;
+        open();
+    }
+
+    private void open(){
+        for(File file : root.listFiles()){
+            if(JournalSegmentFile.isSegmentFile(name,file)){
+                JournalSegmentFile journalSegmentFile = new JournalSegmentFile(file);
+                JournalSegment<E> journalSegment = new JournalSegment<E>(journalSegmentFile,null);
+            }
+
+        }
+    }
 }

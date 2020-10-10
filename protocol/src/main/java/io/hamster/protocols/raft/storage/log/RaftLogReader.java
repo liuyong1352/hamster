83fe16076e0c24021821243d9cc0443dd14046ad
andre <bjliuyong@jd.com>
2020-10-10 17:11:55 +0800

    add raft log storage

--- protocol/src/main/java/io/hamster/protocols/raft/storage/log/RaftLogReader.java
+++ protocol/src/main/java/io/hamster/protocols/raft/storage/log/RaftLogReader.java
@@ -3,6 +3,9 @@ package io.hamster.protocols.raft.storage.log;
 import io.hamster.storage.journal.DelegatingJournalReader;
 import io.hamster.storage.journal.JournalReader;
 
+/**
+ * Raft log reader
+ */
 public class RaftLogReader extends DelegatingJournalReader<RaftLogEntry> {
 
     public RaftLogReader(JournalReader<RaftLogEntry> delegate) {
--- protocol/src/main/java/io/hamster/protocols/raft/storage/log/RaftLogWriter.java
+++ protocol/src/main/java/io/hamster/protocols/raft/storage/log/RaftLogWriter.java
@@ -3,6 +3,9 @@ package io.hamster.protocols.raft.storage.log;
 import io.hamster.storage.journal.DelegatingJournalWriter;
 import io.hamster.storage.journal.JournalWriter;
 
+/**
+ * Raft log writer
+ */
 public class RaftLogWriter extends DelegatingJournalWriter<RaftLogEntry> {
 
     public RaftLogWriter(JournalWriter<RaftLogEntry> delegate) {
